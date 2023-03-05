f20b344874c2061ce8f739d0ac5a58afca801007
nagy marcell <mnagy@mscluster-login1.ms.wits.ac.za>
2023-03-05 19:22:00 +0200

    Add singularity test

--- bash.sh
+++ bash.sh
@@ -12,4 +12,8 @@ cat /etc/lsb-release
 
 # Test python 
 python3 py.py  # Python script
-jupyter nbconvert --execute --to notebook --inplace testBook.ipynb  # Python notebook
+#jupyter nbconvert --execute --to notebook --inplace testBook.ipynb  # Python notebook
+
+# Test singularity
+singularity pull ubuntu.sif library://ubuntu:20.04
+singularity exec ./ubuntu.sif cat /etc/lsb-release
