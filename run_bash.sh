d6e40f48a431d5339548772a5fcaf9b19b5c802f
nagy marcell <mnagy@mscluster-login1.ms.wits.ac.za>
2023-03-05 12:21:02 +0200

    Correct file naming

--- run_bash.sh
+++ run_bash.sh
@@ -2,4 +2,4 @@
 var1="$(pwd)/results/slurm-%j.err"
 var2="$(pwd)/results/slurm-%j.out"
 var3="stampede"
-sbatch --error=$var1 --output=$var2 --partition=$var3 bashrun.sh
+sbatch --error=$var1 --output=$var2 --partition=$var3 bash.sh
